55a20e05109d4c414c77fd6867b6dcf9468b44d5
Dmitry Fedyuk <dfediuk@gmail.com>
2019-01-09 15:29:56 +0700

    no message

--- Observer/DataProvider/SearchResult.php
+++ Observer/DataProvider/SearchResult.php
@@ -3,6 +3,7 @@ namespace Inkifi\MissingOrder\Observer\DataProvider;
 use Df\Framework\Plugin\View\Element\UiComponent\DataProvider\DataProvider as Plugin;
 use Magento\Framework\Api\Search\SearchResult as ApiSearchResult;
 use Magento\Framework\Api\Search\SearchResultInterface as ISearchResult;
+use Magento\Framework\DB\Select;
 use Magento\Framework\Event\Observer as Ob;
 use Magento\Framework\Event\ObserverInterface;
 use Magento\Framework\View\Element\UiComponent\DataProvider\DataProvider as Provider;
@@ -23,7 +24,7 @@ final class SearchResult implements ObserverInterface {
 	function execute(Ob $o) {
 		$provider = $o[Plugin::PROVIDER]; /** @var Provider $provider */
 		/** @var ISearchResult|ApiSearchResult|UiSearchResult|OrderGC|InvoiceGC|CreditmemoGC $result */
-		$result = $o[Plugin::RESULT];
+		$r = $o[Plugin::RESULT];
 		// 2017-08-02 For now, we do not handle «sales_order_invoice_grid_data_source»
 		// and «sales_order_creditmemo_grid_data_source».
 		if ('sales_order_grid_data_source' === $provider->getName()) {
@@ -33,8 +34,14 @@ final class SearchResult implements ObserverInterface {
 			 * @see \Magento\Framework\View\Element\UiComponent\DataProvider\SearchResult::$document
 			 * Структура документа описана здесь: https://mage2.pro/t/1908
 			 */
-			df_map($result, function(Document $item) {
-				$item['df_class'] = 'complete';
+			$oids = df_int(df_map($r, function(Document $i) {return $i['entity_id'];})); /** @var int[] $oids */
+			$select = df_db_from('mediaclip_orders', ['magento_order_id', 'id']); /** @var Select $select */
+			$select->where('magento_order_id IN (?)', $oids);
+			$map = df_conn()->fetchPairs($select); /** @var array(Magento order ID => Mediaclip order ID) $map */
+			df_map($r, function(Document $i) use($map) {
+				if (!isset($map[intval($i['entity_id'])])) {
+					$i['df_class'] = 'df-missed';
+				}
 			});
 		}
 	}
