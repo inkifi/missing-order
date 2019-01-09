43534e83f9b6fb8e638c6466c14592e8938371d4
Dmitry Fedyuk <dfediuk@gmail.com>
2019-01-09 16:32:18 +0700

    1.0.1

--- Observer/DataProvider/SearchResult.php
+++ Observer/DataProvider/SearchResult.php
@@ -39,7 +39,10 @@ final class SearchResult implements ObserverInterface {
 			$select->where('magento_order_id IN (?)', $oids);
 			$map = df_conn()->fetchPairs($select); /** @var array(Magento order ID => Mediaclip order ID) $map */
 			df_map($r, function(Document $i) use($map) {
-				if ('canceled' !== $i['status'] && !isset($map[intval($i['entity_id'])])) {
+				if (
+					!in_array($i['status'], ['canceled', 'pending_payment'])
+					&& !isset($map[intval($i['entity_id'])])
+				) {
 					$i['df_class'] = 'df-missed';
 				}
 			});
--- view/adminhtml/web/listing.js
+++ view/adminhtml/web/listing.js
@@ -1,7 +1,5 @@
+var i = 0;
 define (['Magento_Ui/js/grid/listing'], function (listing) {'use strict'; return listing.extend ({
 	defaults: {template: 'Inkifi_MissingOrder/listing'}
-	,getRowClass: function(r) {
-		debugger;
-		return r['df_class'];
-	}
+	,getRowClass: function(r) {return r['df_class'] ? r['df_class'] : (i++ % 2 ? '_odd-row' : '');}
 });});
